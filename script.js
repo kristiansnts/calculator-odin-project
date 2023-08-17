b6f898ddbe47192c3d0b2b26ac1892ce69fba64d
kristiansnts <epafroditus.kritian@gmail.com>
2023-08-17 18:16:13 +0700

    fix bug non numeric button on screen

---
+++ script.js
@@ -0,0 +1,28 @@
+const screen = document.querySelector('.screen');
+const screenText = screen.querySelector('h1');
+
+const board = document.querySelector('.board');
+const buttons = board.querySelectorAll('button[type="button"]');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const className = button.classList;
+        let num = button.textContent;
+        if(className.contains('delete') || className.contains('reset') || className.contains('trigger')){
+            return;
+        }
+        if(!screenText.textContent && button.classList.contains('operator')){
+            num = '';
+        }
+        screenText.textContent += num;
+    })
+})
+
+const deleteButton = board.querySelector('.delete');
+deleteButton.addEventListener('click', () => {
+    if(!screenText.textContent){
+        return;
+    }
+    console.log(screenText.textContent.split(' '));
+})
+
+console.log(parseFloat('.09'))
