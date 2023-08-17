47f300d7a7171fcfb388bde5ceadb99baeb2566f
kristiansnts <epafroditus.kritian@gmail.com>
2023-08-17 20:08:49 +0700

    add calculate number logic

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const screen = document.querySelector('.screen');
 const screenText = screen.querySelector('h1');
-
 const board = document.querySelector('.board');
 const buttons = board.querySelectorAll('button[type="button"]');
+const deleteButton = board.querySelector('.delete');
+const resetButton = board.querySelector('.reset');
+const getResult = board.querySelector('.trigger');
+
+let isCalculated = false;
+
 buttons.forEach(button => {
     button.addEventListener('click', () => {
+
+        if(isCalculated){
+            screenText.textContent = ""
+            isCalculated = false;
+        }
+
         const className = button.classList;
         let num = button.textContent;
         if(className.contains('delete') || className.contains('reset') || className.contains('trigger')){
@@ -19,7 +30,6 @@ buttons.forEach(button => {
 
 function removeOnScreen(buttonTrigger){
     let contents = "";
-    console.log(buttonTrigger.classList.contains('delete'))
     if(buttonTrigger.classList.contains('delete')){
         contents = screenText.textContent.slice(0, -1);
         screenText.textContent = contents;
@@ -27,8 +37,24 @@ function removeOnScreen(buttonTrigger){
     screenText.textContent = contents
 }
 
-const deleteButton = board.querySelector('.delete');
-const resetButton = board.querySelector('.reset');
+function Calculator(){
+    this.methods = {
+        "+": (a, b) => a + b,
+        "-": (a, b) => a - b,
+        "*": (a, b) => a * b,
+        "/": (a, b) => a / b
+    }
+
+    this.calculate = (str) => {
+        let val = str.split(' '), 
+        a = +(val[0]),
+        op = val[1],
+        b = +(val[2]);
+
+        return this.methods[op](a,b)
+    }
+}
+
 deleteButton.addEventListener('click', (e) => {
     if(!screenText.textContent){
         return;
@@ -45,5 +71,11 @@ resetButton.addEventListener('click', (e) => {
     removeOnScreen(buttonTrigger)
 })
 
-console.log(screenText.textContent.split(' '));
-console.log(parseFloat('.09'))
+getResult.addEventListener('click', function(){
+    let calc = new Calculator;
+    let getContent = screenText.textContent;
+    let result = calc.calculate(getContent)
+    screenText.textContent = (String(result).includes('.')) ? result.toFixed(2) : result;
+    isCalculated = true;
+})
+
